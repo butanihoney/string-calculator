3859161acf1fe927ec886027b0d2cbc6ff86f5b4
honey <honeybutani2000@gmail.com>
2024-09-20 00:05:25 +0530

    Added the first test case to calculate sum from a comma separated string

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import StringCalculator from './component/StringCalculator/StringCalculator'
+import './App.css'
+
+function App() {
+
+  return (
+   <StringCalculator/> 
+   )
+}
+
+export default App
